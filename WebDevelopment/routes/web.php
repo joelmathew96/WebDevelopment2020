6ff374f2d8ec41daf7f9a5b44f24c14e2ebee2ef
joelmathew96 <66008071+joelmathew96@users.noreply.github.com>
2021-01-13 00:24:47 +0800

    Update web.php

--- WebDevelopment/routes/web.php
+++ WebDevelopment/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +17,31 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+   
 });
 
-Auth::routes();
+Route::post('/search', function(){
+    $q = Input::get('q');
+    dd($q);
+});
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/logout', function () {
+    Session::forget('user');
+    return redirect('login');
+});
+
+Route::post("/login",[UserController::class,'login']);
+Route::get("/",[ProductController::class,'index']);
+Route::get("detail/{id}",[ProductController::class,'detail']);
+Route::get("search",[ProductController::class,'search']);
+Route::post("add_to_cart",[ProductController::class,'addToCart']);
+Route::get("cartlist",[ProductController::class,'cartList']); 
+Route::get("removecart/{id}",[ProductController::class,'removeCart']); 
+Route::get("ordernow",[ProductController::class,'orderNow']); 
+Route::post("orderplace",[ProductController::class,'orderPlace']);
+Route::get("myorders",[ProductController::class,'myOrders']);
+ 
